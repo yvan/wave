49f90128adc3445b3db83128175dd6ad5d2d2130
yvanscher <yvanscher@gmail.com>
2014-09-19 20:49:55 -0400

    pre hacking setup commit

---
+++ wave/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  wave
+//
+//  Created by Yvan Scher on 9/19/14.
+//  Copyright (c) 2014 Yvan Scher. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
